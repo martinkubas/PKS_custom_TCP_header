194fa11220086d891f21805d3938d2914ec5060f
Martin Kubis <68854284+martinkubas@users.noreply.github.com>
2024-11-08 19:55:25 +0100

    keepalive, headerstruct in dif file

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 import socket
 import threading
 import struct
-
-
-HEADER_SIZE = 16 #velkost v bajtoch
-
-
-FLAG_SYN = 0b00000001       # SYN = 1
-FLAG_SYN_ACK = 0b00000010   # SYN-ACK = 2
-FLAG_ACK = 0b00000011       # ACK = 3
-FLAG_FIN = 0b11111111       # FIN = 255
-
-
-
-# I = 32-bit seq number, ack number
-# H = 16-bit window, length
-# B = 8-bit flags
-# H = 16-bit checksum
-# 3B = 24-bit offset - nech to je 16B dokopy
-HEADER_FORMAT = '!IIHHBHB'
-
-
-
+import headerStructure
+import time
 
 LOCAL_PORT = int(input("Zadajte váš lokálny port: "))
 PEER_PORT = int(input("Zadajte port pre druhú stranu: "))
@@ -40,18 +21,21 @@ sock.bind((LOCAL_IP, LOCAL_PORT))
 is_terminated = False
 connection_established = False
 
+LAST_RECEIVED_MSG_TIME = time.time()
+KEEPALIVE_INTERVAL = 5
+KEEPALIVE_THRESHOLD = 15
 def create_header(seq_num, ack_num, window, length, flags, checksum):   #vytvori hlavicku protokolu
     offset = 0b00000000 #8bit offset
-    return struct.pack(HEADER_FORMAT, seq_num, ack_num, window, length, flags, checksum, offset)
+    return struct.pack(headerStructure.HEADER_FORMAT, seq_num, ack_num, window, length, flags, checksum, offset)
 
 
 
 def parse_header(packet):   #funkcia na rozbalenie hlavicky
-    return struct.unpack(HEADER_FORMAT, packet[:HEADER_SIZE])
+    return struct.unpack(headerStructure.HEADER_FORMAT, packet[:headerStructure.HEADER_SIZE])
 
 def establish_connection():
     if not connection_established:
-        header = create_header(0, 0,0, 0, FLAG_SYN, 0)  #prazdny packet s flagom SYN
+        header = create_header(0, 0,0, 0, headerStructure.header_flags.SYN, 0)  #prazdny packet s flagom SYN
         sock.sendto(header, (PEER_IP, PEER_PORT))
     else:
         print("uz sme spojeni")
@@ -60,7 +44,7 @@ def end_connection():
     global is_terminated, connection_established
     if connection_established:
         print("spojenie ukoncene!")
-        header = create_header(0, 0, 0,0,FLAG_FIN, 0)   #prazdny packet s ukoncovacim flagom
+        header = create_header(0, 0, 0,0,headerStructure.header_flags.FIN, 0)   #prazdny packet s ukoncovacim flagom
         sock.sendto(header, (PEER_IP, PEER_PORT))
         connection_established = False
 
@@ -82,58 +66,80 @@ def send_messages(message): #posle text spravu peerovi
 
 
 def receive_messages(): #funkcia na prijmanie sprav
-    global is_terminated, connection_established
+    global is_terminated, connection_established, LAST_RECEIVED_MSG_TIME
 
     while not is_terminated:
         try:
             sock.settimeout(1)  #kazdu sekundu sa program pozrie ci is_terminated neni uz false aby nebol zaseknuty v recvfrom() navzdy
             data, addr = sock.recvfrom(1024)  # Prijímanie až do 1024 bajtov
-            body = data[HEADER_SIZE:]
+            body = data[headerStructure.HEADER_SIZE:]
 
             seq_num, ack_num, window, length, flags, checksum, offset = parse_header(data)
 
 
-            if flags == FLAG_SYN and not connection_established:
+            if flags == headerStructure.header_flags.SYN and not connection_established:
                 print(f"\nReceived SYN from {addr}. Sending SYN-ACK...")
-                header = create_header( 0,0, 0,0, FLAG_SYN_ACK, 0)
+                header = create_header( 0,0, 0,0, headerStructure.header_flags.SYN_ACK, 0)
                 sock.sendto(header, addr)
 
-            elif flags == FLAG_SYN_ACK and not connection_established:
+            elif flags == headerStructure.header_flags.SYN_ACK and not connection_established:
                 print(f"\nReceived SYN-ACK from {addr}. Sending ACK to complete handshake...")
-                header = create_header(0, 0,0,0, FLAG_ACK, 0)
+                header = create_header(0, 0,0,0, headerStructure.header_flags.ACK, 0)
                 sock.sendto(header, addr)
                 connection_established = True
+                LAST_RECEIVED_MSG_TIME = time.time()
                 print("Connection established!")
 
-            elif flags == FLAG_ACK and not connection_established:
+            elif flags == headerStructure.header_flags.ACK and not connection_established:
                 print("Received ACK. Connection established!")
+                LAST_RECEIVED_MSG_TIME = time.time()
                 connection_established = True
 
             elif connection_established:
-                if flags == FLAG_FIN:   #ak dojde ukoncovaci packet
+                if flags == headerStructure.header_flags.FIN:   #ak dojde ukoncovaci packet
                     print("Spojenie ukoncene druhou stranou!")
                     connection_established = False
 
+                elif flags == headerStructure.header_flags.KEEPALIVE:
+                    header = create_header(0, 0,0, 0,headerStructure.header_flags.KEEPALIVE_ACK, 0)
+                    sock.sendto(header, addr)
+                    LAST_RECEIVED_MSG_TIME = time.time()
+                    print("dostal som keepalive")
 
+                elif flags == headerStructure.header_flags.KEEPALIVE_ACK:
+                    print("dostal som keepalive ack.. teraz je allgood")
+                    LAST_RECEIVED_MSG_TIME = time.time()
 
                 if length > 0:
                     message = body.decode()
                     print(f"\nSpráva od {addr}: {message}")
+                    LAST_RECEIVED_MSG_TIME = time.time()
 
         except socket.timeout: #kazdu sekundu sa znovu prejde cez loop
             continue
         except socket.error as e:
-            if hasattr(e, 'winerror') and e.winerror == 10054:  #error ked este druhy pouzivatel nema nastavene porty
-                connection_established = False #pre pripad ze jeden pouzivatel nevypne program spravne
-                print("Druha strana este nieje zapojena!")
-            else:
                 print(f"Chyba pri prijímaní správy: {e}")
+
+def keepalive():
+    global connection_established
+    while not is_terminated:
+        if connection_established and time.time() - LAST_RECEIVED_MSG_TIME > KEEPALIVE_THRESHOLD:
+            connection_established = False
+            print("dlho mi nedosla ziadna sprava, spojenie povazujem za mrtve")
+            break
+        elif connection_established and time.time() - LAST_RECEIVED_MSG_TIME > KEEPALIVE_INTERVAL:
+            header = create_header(0, 0, 0, 0, headerStructure.header_flags.KEEPALIVE, 0)
+            sock.sendto(header, (PEER_IP, PEER_PORT))
+
+        time.sleep(1)
 
 
 def main():
     global is_terminated
     receive_thread = threading.Thread(target=receive_messages)
     receive_thread.start()
+    keepalive_thread = threading.Thread(target=keepalive)
+    keepalive_thread.start()
     print("------------------------\n"
           "Ak chcete: \n"
           "poslat subor(nefunkcne) -> stlacte \"f\"\n"
@@ -166,6 +172,7 @@ def main():
         else:
             send_messages(choice)
 
+    keepalive_thread.join()
+
 if __name__ == "__main__":
     main()
-
