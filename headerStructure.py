e9c4f78e07b8110cd4928b3c6d339fbedc798712
Martin Kubis <68854284+martinkubas@users.noreply.github.com>
2024-11-20 14:58:07 +0100

    flags

--- headerStructure.py
+++ headerStructure.py
@@ -11,10 +11,19 @@ class Flags():
     def __init__(self):
         self.SYN =              0b00000001
         self.SYN_ACK =          0b00000010
+
         self.ACK =              0b00000011
+        self.NACK =             0b11111100
+
         self.FIN =              0b11111111
+        self.FIN_ACK =          0b11111110
+
         self.KEEPALIVE =        0b10000000
-        self.KEEPALIVE_ACK =    0b10000001
+        self.KEEPALIVE_ACK =    0b11000000
 
+        self.START =            0b11110000
+        self.STOP =             0b00001111
 
+        self.FRAG =             0b11001100
+        self.FRAGSTOP =         0b00110011
 header_flags = Flags()
